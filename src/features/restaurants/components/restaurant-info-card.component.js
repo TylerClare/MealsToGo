13f87d2eea9b486d8c9acae08d452ac679773682
Tyler Clare <tyclare@hotmail.com>
2021-03-27 22:09:45 -0400

    Added Theme

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,11 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
 const Title = styled.Text`
-    padding: 16px;
+    font-family: ${(props) => props.theme.fonts.heading};
+    font-size: ${(props) => props.theme.fontSizes.body};
+    color: ${(props) => props.theme.colors.ui.primary};
 `;
+
+ const RestaurantCard = styled(Card)`
+    background-color: ${(props) => props.theme.colors.bg.primary};
+ `;
+
+ const RestaurantCardCover = styled(Card.Cover)`
+    padding: ${(props) => props.theme.space[3]};
+    background-color: ${(props) => props.theme.colors.bg.primary};
+ `;
+
+ const Info = styled.View`
+    padding: ${(props) => props.theme.space[3]};
+ `;
+
+ const Address = styled.Text`
+    font-family: ${(props) => props.theme.fonts.body};
+    font-size: ${(props) => props.theme.fontSizes.caption};
+ `;
 
 export const RestaurantInfoCard = ({ restaurant={} }) => {
     
@@ -15,21 +34,19 @@ export const RestaurantInfoCard = ({ restaurant={} }) => {
         photos = [
             "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
         ],
-        address = "100 some randome street",
+        address = "100 some random street",
         isOpenNow = true,
         rating = 4,
         isClosedTemporarily
     } = restaurant;
     
     return (
-        <Card elavation={5} style={styles.card}>
-            <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-            <Title style={styles.title}>{name}</Title>
-        </Card>
+        <RestaurantCard elavation={5}>
+            <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+            <Info>
+                <Title>{name}</Title>
+                <Address>{address}</Address>
+            </Info>
+        </RestaurantCard>
     );
 };
-
-const styles = StyleSheet.create({
-    card: { backgroundColor: "white" },
-    cover: { padding: 20, backgroundColor: "white" }
-})
