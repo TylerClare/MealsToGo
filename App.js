bffac8f233b02f1816956c5f7fb3465825a9f49d
Tyler Clare <tyclare@hotmail.com>
2021-04-04 11:18:40 -0400

    Added Context Provider and Mocks

--- App.js
+++ App.js
@@ -19,6 +19,8 @@ import {
 
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+
 
 const Tab = createBottomTabNavigator();
 
@@ -58,19 +60,21 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator
-            screenOptions={ createScreenOptions }
-            tabBarOptions={{
-              activeTintColor: "tomato",
-              inactiveTintColor: "gray",
-            }}
-          >
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen}/>
-            <Tab.Screen name="Map" component={Map}/>
-            <Tab.Screen name="Settings" component={Settings}/>
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator
+              screenOptions={ createScreenOptions }
+              tabBarOptions={{
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray",
+              }}
+            >
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen}/>
+              <Tab.Screen name="Map" component={Map}/>
+              <Tab.Screen name="Settings" component={Settings}/>
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
         <ExpoStatusBar style="auto" />
       </ThemeProvider>
     </>
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
@@ -17,20 +18,25 @@ const RestaurantList = styled(FlatList).attrs({
   }
 })``;
 
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchContainer>
-        <Searchbar />
-    </SearchContainer>
-    <RestaurantList 
-      data={[{name: 1}, {name: 2},{name: 3}, {name: 4},{name: 5}, {name: 6},{name: 7}, {name: 8},{name: 9}, {name: 10},{name: 11}, {name: 12},{name: 13}, {name: 14}]}
-      renderItem={() => 
-      <>
-        <Spacer position="bottom" size="large">
-          <RestaurantInfoCard />
-        </Spacer>
-      </>}
-      keyExtractor={(item) => item.name}
-    />  
-  </SafeArea>
-);
+export const RestaurantsScreen = () => {
+
+  const restaurantContext = useContext(RestaurantsContext);
+
+  return (
+    <SafeArea>
+      <SearchContainer>
+          <Searchbar />
+      </SearchContainer>
+      <RestaurantList 
+        data={restaurantContext.restaurants}
+        renderItem={() => 
+        <>
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        </>}
+        keyExtractor={(item) => item.name}
+      />  
+    </SafeArea>
+  );
+};
